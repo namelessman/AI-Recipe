041afd2006273781e41a11b85f041a973c500df8
namelessman <zhoumq.good@gmail.com>
2024-10-08 01:37:36 +1300

    🚀 init project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { TextArea, Card } from "@radix-ui/themes";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>AI Recipe</h1>
+      <Card>
+        <TextArea />
+      </Card>
+      <Card>
+        <TextArea />
+      </Card>
+    </>
+  );
+}
+
+export default App;
